afd32f8f87448079e88282f0c134df4f1569008b
pallomagualter <pallomagualter@gmail.com>
2021-04-29 12:02:35 -0300

    install cors and explanations

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -3,9 +3,9 @@ import db from '../database/connection';
 
 export default class ConnectionsController {
   async index(request: Request, response: Response) {
-    const totalConnections = await db('connections').count('* as total');
+    const totalConnections = await db('connections').count('* as total'); //as é o alias para atribuir nome da tabela
 
-    const { total } = totalConnections[0];
+    const { total } = totalConnections[0]; //como sabe que vai retornar sempre uma linha é só pegar a primeira posição do array
 
     return response.json({ total });
   }
