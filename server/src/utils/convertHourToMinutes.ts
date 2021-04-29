02733a2965e2daee3f95db7df73632e6dccc88b2
pallomagualter <pallomagualter@gmail.com>
2021-04-29 10:57:35 -0300

    explanation

--- server/src/utils/convertHourToMinutes.ts
+++ server/src/utils/convertHourToMinutes.ts
@@ -1,5 +1,5 @@
 export default function convertHourToMinutes(time: string) {
-  const [hour, minutes] = time.split(':').map(Number); //descontrução para hora e minuto
+  const [hour, minutes] = time.split(':').map(Number); //descontrução para hora e minuto com split no : e o map transformando em tipo number
   const timeInMinutes = (hour * 60) + minutes;
 
   return timeInMinutes;
